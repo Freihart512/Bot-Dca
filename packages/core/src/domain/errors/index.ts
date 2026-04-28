1d4222a00e04219ab83ab1e7c8544c3612a86ed3
Carlos Gonzalez <carlosfabian512@gmail.com>
2026-04-27 18:03:01 -0600

    [TECH-010] feat(core): DomainError y códigos serializables
    Traceability: TECH-010, US-01, UC-01

---
+++ packages/core/src/domain/errors/index.ts
@@ -0,0 +1,9 @@
+export { DomainErrorCode, type DomainErrorCodeString } from "./codes.js";
+export {
+  type DomainErrorSerializable,
+  DomainError,
+  InvalidArgumentError,
+  InvariantViolationError,
+  ConflictError,
+  NotFoundError
+} from "./domain-error.js";
